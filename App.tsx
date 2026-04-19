72b558966b34429316a1e2007095bed5cb3b849f
FDRkinggon <fokoufdr@gmail.com>
2026-04-19 07:05:35 +0100

    navigation bien faite

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { ScrollView, StyleSheet, Text, TextInput, Button, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Ionicons} from '@expo/vector-icons';
 
 // L'interface représente UN SEUL message
 interface Note {
@@ -7,41 +10,23 @@ interface Note {
   texte: string;
 }
 
-export default function App() {
-  const [data, setData] = useState<Note[]>([]);// 1. Créer une variable pour stocker la liste des messages (vide au début)
-  const [texteSaisi, setTexteSaisi] = useState<string>('');// 2. Créer une variable pour stocker le texte saisi par l'utilisateur
-  const API_URL = 'http://192.168.1.142:8000/api/hello/';// L'URL de l'endpoint Django
 
-  //FONCTION 1: Charger les messages du backend au lancement de l'application
-  const chargerNotes = () => {
+//const [data, setData] = useState<Note[]>([]);// 1. Créer une variable pour stocker la liste des messages (vide au début)
+const API_URL = 'http://192.168.1.174:8000/api/hello/';// L'URL de l'endpoint Django
+
+// Le composant qui affiche la liste des messages
+function HomeScreen() {// Ce composant est utilisé pour afficher la liste des messages dynamiques
+  const [data, setData] = useState<Note[]>([]);
+  
+    useEffect(() => {
     fetch(API_URL)
-      .then(res => res.json())// Convertir la réponse en JSON
-      .then(json => setData(json))// Mettre à jour la variable "data" avec la liste des messages reçue du backend
-      .catch(err => console.error(err));
-  };
-
-  //FONCTION 2: Envoyer un nouveau message au backend
-  const envoyerNote = () => {
-    if (texteSaisi.length === 0) return; // Ne rien faire si le champ de saisie est vide
-    fetch(API_URL, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ contenu: texteSaisi }) // Envoyer le texte saisi dans le corps de la requête
-    })
-    .then(() =>{
-      setTexteSaisi(''); // Vider le champ de saisie après l'envoi
-      chargerNotes(); // Recharger la liste des messages pour afficher le nouveau message
-    })
-    .catch(err => console.error(err));
-  };
-
-  useEffect(() => {
-    chargerNotes(); // Charger les messages du backend au lancement de l'application
+      .then(res => res.json()).then(json => setData(json)).catch(err => console.error(err));
   }, []);
 
   return (
     <View style={styles.container}>
-      <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.title}>Sentinelle</Text>
+      <ScrollView style={{width: '100%'}} contentContainerStyle={styles.container}>
         <Text style={styles.title}>Mes Messages Dynamiques</Text>
         
         {/* LA BOUCLE MAGIQUE : Pour chaque "item" dans "data" */}
@@ -52,7 +37,30 @@ export default function App() {
         ))}
         
       </ScrollView>
+    </View>
+  );
+}
 
+// Le composant qui permet de saisir un nouveau message
+function SettingsScreen() {
+  const [texteSaisi, setTexteSaisi] = useState<string>('');
+
+  const envoyerNote = () => {
+    fetch(API_URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ contenu: texteSaisi }) // Envoyer le texte saisi au backend
+    })
+    .then(() => {
+      setTexteSaisi(''); // Vider le champ de saisie après l'envoi
+      alert('Note envoyée avec succès !'); // Recharger la liste des messages pour afficher la nouvelle note
+    })
+    .catch(err => console.error(err));
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Settings</Text>
       {/* 3. Le composant de saisie */}
       <TextInput
         style={styles.input}
@@ -63,6 +71,42 @@ export default function App() {
       
       <Button title="Envoyer" onPress={envoyerNote} />
     </View>
+  );
+}
+
+function ProfileScreen() {
+  return (
+    <View style={styles.screen}>
+      <Ionicons name="person-circle" size={100} color="blue" />
+      <Text style={styles.title}>Profile Screen</Text>
+      <Text style={{fontSize: 16 }}>Utilisateur: FDRkinggon</Text>
+      <Text style={{color: 'grey'}}>Status: Developpeur</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator 
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({color, size}) => {
+            let iconName: any;
+            if(route.name === 'Accueil') iconName = 'home';
+            else if (route.name === 'Parems') iconName = 'settings';
+            else if (route.name === 'Profile') iconName = 'person';
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'blue',
+          tabBarInactiveTintColor: 'plum',
+        })} >
+        <Tab.Screen name="Accueil" component={HomeScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen name="Parems" component={SettingsScreen} />
+        </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -85,5 +129,12 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     borderRadius: 8,
     marginTop: 10,
+  },
+  screen: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#f0f0f0',
   },
 });
